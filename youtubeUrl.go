e9301113277154e3722405d3ed004e108becb1be
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-12-11 13:59:31 +0100

    Updated regex to check for video id length

--- youtubeUrl.go
+++ youtubeUrl.go
@@ -6,6 +6,6 @@ import (
 
 // Validate youtube url
 func Valid(url string) bool {
-	m, _ := regexp.MatchString(`((http://)?)(www\.)?((youtube\.com/)|(youtu.be)).+`, url)
+	m, _ := regexp.MatchString(`((?:http://)?)(?:www\.)?(?:(youtube\.com/(\/watch\?(?:\=.*v=((\w|-){11}))|.+))|(youtu.be\/\w{11}))`, url)
 	return m
 }
