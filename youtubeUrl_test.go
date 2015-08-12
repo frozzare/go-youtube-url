733bc4e1a1a63d0a8d77a52225e983e8a35e762f
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-12 09:45:47 +0200

    Change to frozzare/go-assert

--- youtubeUrl_test.go
+++ youtubeUrl_test.go
@@ -1,12 +1,12 @@
 package youtubeUrl
 
 import (
-	"github.com/bmizerany/assert"
 	"testing"
+
+	"github.com/frozzare/go-assert"
 )
 
 func TestYoutubeUrls(t *testing.T) {
-	// Should be true
 	assert.Equal(t, true, Valid("https://www.youtube.com/watch?v=Xq7z6WpeB0w"))
 	assert.Equal(t, true, Valid("https://youtube.com/watch?v=Xq7z6WpeB0w"))
 	assert.Equal(t, true, Valid("http://www.youtube.com/watch?v=Xq7z6WpeB0w"))
@@ -16,7 +16,6 @@ func TestYoutubeUrls(t *testing.T) {
 	assert.Equal(t, true, Valid("http://youtu.be/Xq7z6WpeB0w"))
 	assert.Equal(t, true, Valid("www.youtube.com/Xq7z6WpeB0w"))
 
-	// Should be false
 	assert.Equal(t, false, Valid("https://www.example.com/watch?v=Xq7z6WpeB0w"))
 	assert.Equal(t, false, Valid("http://youtube/Xq7z6WpeB0w"))
 	assert.Equal(t, false, Valid("youtube/Xq7z6WpeB0w"))
